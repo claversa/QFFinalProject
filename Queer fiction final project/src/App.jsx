3e2d2d78930b3149860d0aba748f22a04b6096df
claversa <claversa@stevens.edu>
2024-11-18 17:00:52 -0500

    project skeleton

---
+++ Queer fiction final project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Homepage from './pages/Homepage'
+
+function App() {
+
+  return (
+    <>
+      <Homepage />
+    </>
+  )
+}
+
+export default App
---
+++ Queer fiction final project/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <div className='flex flex-col items-center'>
+            <h2 className='font-bold text-xl'>Queer Fiction Final Project</h2>
+            <h4>By Caroline LaVersa & Kailey Totland</h4>
+        </div>
+    )
+}
+
+export default Homepage
